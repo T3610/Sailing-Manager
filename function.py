d1b03fbe3e3f195e5c657e154a87fc9691405861
T3610 <toby.h.broadbent@gmail.com>
2021-03-06 17:49:22 +0000

    changes

--- function.py
+++ function.py
@@ -77,7 +77,7 @@ def outOftimeSignUp():
     timeNow = now.strftime("%H:%M:%S")
     
     cutofftime = mycursor.fetchone()
-    cutofftime = cutofftime[0]ll
+    cutofftime = cutofftime[0]
     cutofftime = cutofftime.strftime("%H:%M:%S")
     print(timeNow)
     print(cutofftime)
