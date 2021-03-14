44060a3412ce4bdc1b6eae16b7fc92faa5f65072
T3610 <toby.h.broadbent@gmail.com>
2021-03-14 20:22:55 +0000

    Load table from api

--- static/Results.js
+++ static/Results.js
@@ -26,3 +26,23 @@ function handleFinish(id){
         location.reload();
       });
 }
+
+function updateTable(){
+    var settings = {
+        "url": "http://ec2-35-178-146-200.eu-west-2.compute.amazonaws.com/api/results",
+        "method": "GET",
+        "timeout": 0,
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        $.each(response, function(i, item) {
+            var $tr = $('<tr>').append(
+                $('<td>').text(item.rank),
+                $('<td>').text(item.content),
+                $('<td>').text(item.UID)
+            );
+            console.log($tr.wrap('<p>').html());
+        });
+      });
+}
