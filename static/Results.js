c71e139ef88eb008f73ccdc6b403e04cbb38893f
Benjamin Broadbent <benjamin.d.broadbent@gmail.com>
2021-03-19 20:00:14 +0000

    updated results page to hide lap btn when finished

--- static/Results.js
+++ static/Results.js
@@ -1,94 +1,117 @@
-document.onload = updateTable()
+document.onload = updateTable();
 
-function handleAddLap(id){
-    console.log(id)
+function handleAddLap(id) {
+    console.log(id);
 
     var settings = {
-        "url": "https://racing.dorchestersailingclub.org.uk//addlap/"+id,
-        "method": "PATCH",
-        "timeout": 0,
-      };
-      
-      $.ajax(settings).done(function (response) {
+        url: "https://racing.dorchestersailingclub.org.uk//addlap/" + id,
+        method: "PATCH",
+        timeout: 0,
+    };
+
+    $.ajax(settings).done(function(response) {
         console.log(response);
-        updateTable()
-      });
+        updateTable();
+    });
 }
-function handleFinish(id){
-    console.log(id)
+
+function handleFinish(id) {
+    console.log(id);
 
     var currentTime = Math.round(Date.now() / 1000);
 
     var settings = {
-        "url": "https://racing.dorchestersailingclub.org.uk/finish/"+id+"?finishTime="+currentTime,
-        "method": "PATCH",
-        "timeout": 0,
-      };
-      
-      $.ajax(settings).done(function (response) {
+        url: "https://racing.dorchestersailingclub.org.uk/finish/" +
+            id +
+            "?finishTime=" +
+            currentTime,
+        method: "PATCH",
+        timeout: 0,
+    };
+
+    $.ajax(settings).done(function(response) {
         console.log(response);
-        updateTable()
-      });
+        updateTable();
+    });
 }
 
-function handleRemoveLap(id){
+function handleRemoveLap(id) {
     console.log(id);
     var settings = {
-        "url": "https://racing.dorchestersailingclub.org.uk/removelap/"+id,
-        "method": "PATCH",
-        "timeout": 0,
-      };
-      
-      $.ajax(settings).done(function (response) {
+        url: "https://racing.dorchestersailingclub.org.uk/removelap/" + id,
+        method: "PATCH",
+        timeout: 0,
+    };
+
+    $.ajax(settings).done(function(response) {
         console.log(response);
-        updateTable()
-      });
+        updateTable();
+    });
 }
 
-function handleUnfinish(id){
+function handleUnfinish(id) {
     console.log(id);
     var settings = {
-        "url": "https://racing.dorchestersailingclub.org.uk/unfinish/"+id,
-        "method": "PATCH",
-        "timeout": 0,
-      };
-      
-      $.ajax(settings).done(function (response) {
+        url: "https://racing.dorchestersailingclub.org.uk/unfinish/" + id,
+        method: "PATCH",
+        timeout: 0,
+    };
+
+    $.ajax(settings).done(function(response) {
         console.log(response);
-        updateTable()
-      });
+        updateTable();
+    });
 }
 
-function updateTable(){
+function updateTable() {
     var settings = {
-        "url": "https://racing.dorchestersailingclub.org.uk/api/results",
-        "method": "GET",
-        "timeout": 0,
-      };
+        url: "https://racing.dorchestersailingclub.org.uk/api/results",
+        method: "GET",
+        timeout: 0,
+    };
 
-      
-      $.ajax(settings).done(function (response) {
+    $.ajax(settings).done(function(response) {
         response = $.parseJSON(response);
         console.log(response);
         $("#resultsTable").empty();
         $.each(response, function(i, item) {
+            const lapBtn =
+                '<div class="btn-group"><button class="btn btn-outline-secondary" onclick=handleAddLap(' +
+                item[0] +
+                ')>ADD LAP</button><button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span><div class="dropdown-menu"><a class="dropdown-item" onclick="handleRemoveLap(' +
+                item[0] +
+                ')">Remove lap</a></div></div>';
+            const lapBtnDisables =
+                '<div class="btn-group"><button disabled class="btn btn-outline-secondary" onclick=handleAddLap(' +
+                item[0] +
+                ')>ADD LAP</button><button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span><div class="dropdown-menu"><a class="dropdown-item" onclick="handleRemoveLap(' +
+                item[0] +
+                ')">Remove lap</a></div></div>';
+            const finishBtn =
+                '<button class="btn btn-outline-secondary" onclick=handleFinish(' +
+                item[0] +
+                ")>FINISH</button>";
+            const finishBtnDisabled =
+                '<div class="btn-group"><button disabled class="btn btn-outline-secondary" onclick=handleFinish(' +
+                item[0] +
+                ')>FINISH</button><button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span><div class="dropdown-menu"><a class="dropdown-item" onclick="handleUnfinish(' +
+                item[0] +
+                ')">Unfinish</a></div></div>';
 
-            const lapBtn = '<div class="btn-group"><button class="btn btn-outline-secondary" onclick=handleAddLap('+item[0]+')>ADD LAP</button><button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span><div class="dropdown-menu"><a class="dropdown-item" onclick="handleRemoveLap('+item[0]+')">Remove lap</a></div></div>';
-            const finishBtn = '<button class="btn btn-outline-secondary" onclick=handleFinish('+item[0]+')>FINISH</button>';
-            const finishBtnDisabled = '<div class="btn-group"><button disabled class="btn btn-outline-secondary" onclick=handleFinish('+item[0]+')>FINISH</button><button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span><div class="dropdown-menu"><a class="dropdown-item" onclick="handleUnfinish('+item[0]+')">Unfinish</a></div></div>';
-
-            var $tr = $('<tr>').append(
-                $('<td>').text(item[0]), //ID
-                $('<td>').text(item[1]), //Helm
-                $('<td>').text(item[2]), //Crew
-                $('<td>').text(item[3]), //Sail No
-                $('<td>').text(item[4]), //Class
-                $('<td>').text(item[5]), //Laps
-                $('<td>').append(lapBtn), //LapBTN
-                (item[6])?$('<td>').append(finishBtnDisabled):$('<td>').append(finishBtn), //finishBTN
+            var $tr = $("<tr>").append(
+                $("<td>").text(item[0]), //ID
+                $("<td>").text(item[1]), //Helm
+                $("<td>").text(item[2]), //Crew
+                $("<td>").text(item[3]), //Sail No
+                $("<td>").text(item[4]), //Class
+                $("<td>").text(item[5]), //Laps
+                item[6] ? $("<td>").append(lapBtnDisables) : $("<td>").append(lapBtn), //finishBTN
+                item[6] ?
+                $("<td>").append(finishBtnDisabled) :
+                $("<td>").append(finishBtn) //finishBTN
             );
-            console.log($tr)
+            console.log($tr);
             $("#resultsTable").append($tr);
         });
-      });
+    });
 }
