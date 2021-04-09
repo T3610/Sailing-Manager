cfda4642ae2966983b2d78434e3ec0b5cb28c850
Benjamin Broadbent <benjamin.d.broadbent@gmail.com>
2021-04-09 19:06:53 +0100

    changes to ood page

--- static/oodPage.js
+++ static/oodPage.js
@@ -1,22 +1,7 @@
-function updateRaceType(racetype) {
-  var settings = {
-    url:
-      "development.dorchestersailingclub.org.uk/api/updateracetype/" + racetype,
-    method: "GET",
-    timeout: 0,
-  };
-
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
-}
-
 function handleHandicapRace() {
   $("#raceLength").hide();
-  updateRaceType(0)
 }
 
 function handlePersuitRace() {
   $("#raceLength").show();
-  updateRaceType(1)
 }
