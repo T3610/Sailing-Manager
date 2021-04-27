d6e1af1411058dccc0a8120843be582676601062
Benjamin Broadbent <benjamin.d.broadbent@gmail.com>
2021-04-27 21:09:56 +0100

    combined name and sail num

--- static/ResultsR1.js
+++ static/ResultsR1.js
@@ -242,7 +242,7 @@ function updateTable() {
 
       var $tr = $("<tr>").append(
         $("<td>").text(), //Helm
-        $("<td>").text((item[3]+" ("+item[1]+")"), //Sail No
+        $("<td>").text(item[3] + " (" + item[1] + ")"), //Sail No
         $("<td>").text(item[4]), //Class
         item[7] ? $("<td>").append(lapBtnDisabled) : $("<td>").append(lapBtn), //lapBTN
         item[7]
