f086632bd17a48df7eb00bc623e9d1a627d1d3c1
Benjamin Broadbent <benjamin.d.broadbent@gmail.com>
2021-10-31 12:12:37 +0000

    changes to .env

--- SailingManager/SailingManager/settings.py
+++ SailingManager/SailingManager/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
