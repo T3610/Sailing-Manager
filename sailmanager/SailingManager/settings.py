2ddf9b7a21a2faab209f50b8c2b5a7ba1d026645
Benjamin Broadbent <benjamin.d.broadbent@gmail.com>
2022-02-07 16:59:51 +0000

    Added logging

--- sailmanager/SailingManager/settings.py
+++ sailmanager/SailingManager/settings.py
@@ -174,7 +174,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'handlers': {
         'file': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.FileHandler',
             'filename': 'logging/debug.log',
         },
@@ -182,7 +182,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['file'],
-            'level': 'DEBUG',
+            'level': 'INFO',
             'propagate': True,
         },
     },
